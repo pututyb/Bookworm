d2e03fa76c1655cff7c34d34218b281c18736ed5
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-02-12 17:02:43 +0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Putut Yusri Bahtiar on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
